7680041e3556f23027a61b536e3be26fa9f14126
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2022-01-02 17:16:37 -0700

    Footer and Contact component added

---
+++ portfolio/src/components/Contact/index.js
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import { validateEmail } from "../../utils/helpers";
+
+function ContactForm() {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const { name, email, message } = formState;
+  const [errorMessage, setErrorMessage] = useState("");
+
+  function handleChange(e) {
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+      console.log(isValid);
+      // isValid conditional statement
+      if (!isValid) {
+        setErrorMessage("Your email is invalid.");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage("");
+      }
+    }
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
+
+  return (
+    <section className="contact">
+      <h1>Contact me</h1>
+      <form id="contact-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            name="name"
+            defaultValue={name}
+            onBlur={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input
+            type="email"
+            name="email"
+            defaultValue={email}
+            onBlur={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            name="message"
+            rows="5"
+            defaultValue={message}
+            onBlur={handleChange}
+          />
+        </div>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        <button type="submit" className="contact-button">
+          Submit
+        </button>
+      </form>
+
+      <h2>
+          I can be reached at the following:
+      </h2>
+      <a href="mailto: hibram@hibram.com" className="block"> hibram@hibram.com</a>
+      <a href="tel:555-555-555" className="block">555-555-555</a>
+      </section>
+  );
+        }
+
+        export default ContactForm;
---
+++ portfolio/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+      <footer>
+        <div className="footer-container">
+            <a href="https://github.com/Hibram007">Github</a>
+            <a href="https://www.linkedin.com/in/hibram-sanchez-garcia-321957145/">linkedin</a>
+        </div>
+        </footer>
+    );
+}
+
+export default Footer;
